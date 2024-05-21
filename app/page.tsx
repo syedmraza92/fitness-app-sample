11a446440e5e04475b69e1ccac5796f0b3b6524a
Syed Mehdi Raza <family@zakaria-imac.lan>
2024-05-21 00:53:38 -0400

    final touches

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import Progress from "./components/progress";
 import Center from "./pages/Center";
 import Left from "./pages/Left";
 
 export default function Home() {
   return (
     <main>
-      <div className="flex gap-x-8">
-        <Left />
+      <div className="flex gap-x-8 bg-[#CECECE] w-[100%] p-36">
+        <div className="mt-10">
+          <Left />
+        </div>
         <Center />
       </div>
     </main>
